d8349e1270c916bff6809137acf6cff64d490412
王宇 <15800388603@163.com>
2018-08-01 11:00:05 +0800

    Update config.js

--- moudle/config.js
+++ moudle/config.js
@@ -6,8 +6,8 @@ var mysql_config = {
     host : '127.0.0.1',
     port: 3306,
     database:'wtiy',
-    user:'wtiy',
-    password:'wangyu1993',
+    user:'xxx',
+    password:'xxx',
     charset:'utf8',
     multipleStatements:true/*查询多条sql*/
 };
